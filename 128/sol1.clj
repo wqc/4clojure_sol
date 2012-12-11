8e33ca41c2958003a66a51cb89e3f03d2e106d89
wqc <sigh1988@qq.com>
2012-12-11 22:16:42 +0800

    add one solution for problem #128

---
+++ 128/sol1.clj
@@ -0,0 +1,7 @@
+(
+	fn sol[ coll ]
+		{	
+			:suit ( { \D :diamond \H :heart \C :club \S :spade }( first coll ) )  
+			:rank ( { \A 12 \2 0 \3 1 \4 2 \5 3 \6 4 \7 5 \8 6 \9 7 \T 8 \J 9 \Q 10 \K 11 } (last coll) ) 
+		} 
+)
